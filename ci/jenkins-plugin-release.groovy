49ee0bb4fb53984247a01c1c87a9d2e7f4eb6be8
joachim.segala <joachim.segala@usharesoft.com>
2019-04-05 11:51:16 +0200

    UFORGE-13453 Add release Jenkinsfile
     - use maven release plugin
     - configure scm connection and repository
    
    Change-Id: I2e435b18c94c25e987282ed33425556272bfe423

---
+++ ci/jenkins-plugin-release.groovy
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2007-2019 UShareSoft, All rights reserved
+ *
+ * @author UShareSoft
+ */
+
+pipeline {
+    agent any
+    stages {
+        stage('Release Plugin') {
+            steps {
+                script {
+                    sshagent(['gerrit']) {
+                        sh 'mvn release:prepare release:perform -DreleaseVersion=${VERSION} -DdevelopmentVersion=${NEXT_VERSION}-SNAPSHOT -Dtag=${VERSION}'
+                    }
+                }
+            }
+            post {
+                always {
+                    script {
+                        def buildPlugin = load 'ci/lib/build-plugin.groovy'
+                        buildPlugin.archive()
+                    }
+                }
+            }
+        }
+    }
+}
