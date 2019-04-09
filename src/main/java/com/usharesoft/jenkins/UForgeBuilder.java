be5e15cb80216089c2fdba1eb65df37ce37a62c4
Paul Cartier <paul.cartier@usharesoft.com>
2019-04-09 10:40:40 +0200

    UFORGE-13363 Add parameters in UForge build step
    - Add url, version, login, password and templatePath parameters in UForge build step
    - Modify unit tests to integrate parameters
    - Make it pipeline compatible
    
    Change-Id: I4bce452272586d15dff064d36ed1dee9f2b64e3e

--- src/main/java/com/usharesoft/jenkins/UForgeBuilder.java
+++ src/main/java/com/usharesoft/jenkins/UForgeBuilder.java
@@ -1,32 +1,124 @@
 package com.usharesoft.jenkins;
 
+import hudson.AbortException;
 import hudson.Launcher;
 import hudson.Extension;
 import hudson.FilePath;
+import hudson.Util;
+import hudson.util.FormValidation;
 import hudson.model.AbstractProject;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tasks.Builder;
 import hudson.tasks.BuildStepDescriptor;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 import java.io.IOException;
+import java.io.PrintStream;
+
 import jenkins.tasks.SimpleBuildStep;
 import org.jenkinsci.Symbol;
 
 public class UForgeBuilder extends Builder implements SimpleBuildStep {
+    private final String url;
+    private final String version;
+    private final String login;
+    private final String password;
+    private final String templatePath;
 
     @DataBoundConstructor
-    public UForgeBuilder() {
+    public UForgeBuilder(String url, String version, String login, String password, String templatePath) {
+        this.url = Util.fixNull(url);
+        this.version = Util.fixNull(version);
+        this.login = Util.fixNull(login);
+        this.password = Util.fixNull(password);
+        this.templatePath = Util.fixNull(templatePath);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getTemplatePath() {
+        return templatePath;
+    }
+
+    private void checkParameters(PrintStream logger) throws AbortException {
+        boolean missingParameter = false;
+        if (url.isEmpty()) {
+            logger.println(Messages.UForgeBuilder_errors_missingUrl());
+            missingParameter = true;
+        }
+        if (version.isEmpty()) {
+            logger.println(Messages.UForgeBuilder_errors_missingVersion());
+            missingParameter = true;
+        }
+        if (login.isEmpty()) {
+            logger.println(Messages.UForgeBuilder_errors_missingLogin());
+            missingParameter = true;
+        }
+        if (password.isEmpty()) {
+            logger.println(Messages.UForgeBuilder_errors_missingPassword());
+            missingParameter = true;
+        }
+        if (templatePath.isEmpty()) {
+            logger.println(Messages.UForgeBuilder_errors_missingTemplatePath());
+            missingParameter = true;
+        }
+        if (missingParameter) {
+            throw new AbortException(Messages.UForgeBuilder_errors_missingParameter());
+        }
     }
 
     @Override
     public void perform(Run<?, ?> run, FilePath workspace, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
+        checkParameters(listener.getLogger());
+
         listener.getLogger().println("UForge AppCenter plugin");
     }
 
     @Extension @Symbol("uforge")
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        public FormValidation doCheckUrl(@QueryParameter String value) {
+            return doCheckField(value);
+        }
+
+        public FormValidation doCheckVersion(@QueryParameter String value) {
+            return doCheckField(value);
+        }
+
+        public FormValidation doCheckLogin(@QueryParameter String value) {
+            return doCheckField(value);
+        }
+
+        public FormValidation doCheckPassword(@QueryParameter String value) {
+            return doCheckField(value);
+        }
+
+        public FormValidation doCheckTemplatePath(@QueryParameter String value) {
+            return doCheckField(value);
+        }
+
+        private FormValidation doCheckField(@QueryParameter String value) {
+            if (value.isEmpty()) {
+                return FormValidation.error(Messages.UForgeBuilder_DescriptorImpl_errors_missingField());
+            }
+            return FormValidation.ok();
+        }
 
         @Override
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
@@ -37,6 +129,5 @@ public class UForgeBuilder extends Builder implements SimpleBuildStep {
         public String getDisplayName() {
             return Messages.UForgeBuilder_DescriptorImpl_displayName();
         }
-
     }
 }
--- src/test/java/com/usharesoft/jenkins/UForgeBuilderTest.java
+++ src/test/java/com/usharesoft/jenkins/UForgeBuilderTest.java
@@ -3,6 +3,8 @@ package com.usharesoft.jenkins;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Label;
+import hudson.model.Result;
+
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
@@ -12,27 +14,33 @@ import org.jvnet.hudson.test.JenkinsRule;
 
 public class UForgeBuilderTest {
 
+    private static final String URL = "http://my-forge.com/api";
+    private static final String VERSION = "3.8.11";
+    private static final String LOGIN = "login";
+    private static final String PASSWORD = "password";
+    private static final String TEMPLATE_PATH = "./template.yml";
+
     @Rule
     public JenkinsRule jenkins = new JenkinsRule();
 
     @Test
-    public void should_project_stay_unchanged_after_roundtrip_configuration() throws Exception {
+    public void should_project_stay_unchanged_and_password_hidden_after_roundtrip_configuration() throws Exception {
         // given
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        project.getBuildersList().add(new UForgeBuilder());
+        project.getBuildersList().add(new UForgeBuilder(URL, VERSION, LOGIN, PASSWORD, TEMPLATE_PATH));
 
         // when
         project = jenkins.configRoundtrip(project);
 
         // then
-        jenkins.assertEqualDataBoundBeans(new UForgeBuilder(), project.getBuildersList().get(0));
+        jenkins.assertEqualDataBoundBeans(new UForgeBuilder(URL, VERSION, LOGIN, "", TEMPLATE_PATH), project.getBuildersList().get(0));
     }
 
     @Test
     public void should_builder_builds_successfully_in_freestyle_project() throws Exception {
         // given
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        project.getBuildersList().add(new UForgeBuilder());
+        project.getBuildersList().add(new UForgeBuilder(URL, VERSION, LOGIN, PASSWORD, TEMPLATE_PATH));
 
         // when
         FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
@@ -47,7 +55,11 @@ public class UForgeBuilderTest {
         WorkflowJob job = jenkins.createProject(WorkflowJob.class, "test-scripted-pipeline");
         String pipelineScript
                 = "node {\n"
-                + "  uforge()\n"
+                + "  uforge url: '" + URL + "', "
+                + "version: '" + VERSION + "', "
+                + "login: '" + LOGIN + "', "
+                + "password: '" + PASSWORD + "', "
+                + "templatePath : '" + TEMPLATE_PATH + "'\n"
                 + "}";
         job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
 
@@ -56,5 +68,26 @@ public class UForgeBuilderTest {
 
         // then
         jenkins.assertLogContains("UForge AppCenter plugin", completedBuild);
+    }
+
+    @Test
+    public void should_build_fails_when_missing_parameters() throws Exception {
+        // given
+        WorkflowJob job = jenkins.createProject(WorkflowJob.class, "test-scripted-pipeline");
+        String pipelineScript
+                = "node {\n"
+                + "  uforge "
+                + "version: '" + VERSION + "', "
+                + "login: '" + LOGIN + "', "
+                + "password: '" + PASSWORD + "', "
+                + "templatePath : '" + TEMPLATE_PATH + "'\n"
+                + "}";
+        job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
+
+        // when
+        WorkflowRun completedBuild = jenkins.assertBuildStatus(Result.FAILURE, job.scheduleBuild2(0));
+
+        // then
+        jenkins.assertLogContains("url: UForge API URL is mandatory.", completedBuild);
     }
 }
